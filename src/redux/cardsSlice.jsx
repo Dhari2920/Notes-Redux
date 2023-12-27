5bc84f5400bfaeabc248c913d41bb83048439a88
Dhari2920 <dhari2920@gmail.com>
2023-12-27 16:20:43 +0530

    updated

--- src/redux/cardsSlice.jsx
+++ src/redux/cardsSlice.jsx
@@ -1,47 +1,55 @@
+
 import { createSlice } from "@reduxjs/toolkit";
 
+// Creating a slice for managing the cards state
 export const cardsSlice = createSlice({
-    name:'cards',
-    initialState:[
+    name: 'cards', // Name of the slice
+    initialState: [
+        // Initial state with sample cards
         {
-            id:1,
-            title:'Feedback',
-            body:'Lorem ipsum dolor sit amet consectetur. Sollicitudin enim risus ut vestibulum morbi tellus sit ac. Fames auctor quisque et aliquam maecenas sed at vitae facilisis. .'        
+            id: 1,
+            title: 'Feedback',
+            body: 'Lorem ipsum dolor sit amet consectetur. Sollicitudin enim risus ut vestibulum morbi tellus sit ac. Fames auctor quisque et aliquam maecenas sed at vitae facilisis. .'
         },
         {
-            id:2,
-            title:'Lyrics',
-            body:'Lorem ipsum dolor sit amet consectetur. Sollicitudin enim risus ut vestibulum morbi tellus sit ac. Fames auctor quisque et aliquam maecenas sed at vitae facilisis. .'    
+            id: 2,
+            title: 'Lyrics',
+            body: 'Lorem ipsum dolor sit amet consectetur. Sollicitudin enim risus ut vestibulum morbi tellus sit ac. Fames auctor quisque et aliquam maecenas sed at vitae facilisis. .'
         },
         {
-            id:3,
-            title:'Weekly Task',
-            body:'Lorem ipsum dolor sit amet consectetur. Sollicitudin enim risus ut vestibulum morbi tellus sit ac. Fames auctor quisque et aliquam maecenas sed at vitae facilisis. .'    
+            id: 3,
+            title: 'Weekly Task',
+            body: 'Lorem ipsum dolor sit amet consectetur. Sollicitudin enim risus ut vestibulum morbi tellus sit ac. Fames auctor quisque et aliquam maecenas sed at vitae facilisis. .'
         }
     ],
-    reducers:{
-        add:(state,action)=>{
+    reducers: {
+        // Reducer function for adding a new card
+        add: (state, action) => {
             const newCard = action.payload;
-            state.push(newCard)
+            state.push(newCard);
         },
-        remove:(state,action)=>{
-           const cardRemove = action.payload;
-           return state.filter((card)=>card.id !== cardRemove);
+        // Reducer function for removing a card
+        remove: (state, action) => {
+            const cardToRemove = action.payload;
+            return state.filter((card) => card.id !== cardToRemove);
         },
-        edit:(state,action)=>{
-            const {id, updateddata }=action.payload;
-            const editedCardIndex = state.findIndex((card) =>card.id === id);
+        // Reducer function for editing a card
+        edit: (state, action) => {
+            const { id, updateddata } = action.payload;
+            const editedCardIndex = state.findIndex((card) => card.id === id);
 
-            if(editedCardIndex !== -1){
-                const editedCard = {...state[editedCardIndex], ...updateddata };
+            if (editedCardIndex !== -1) {
+                const editedCard = { ...state[editedCardIndex], ...updateddata };
                 state[editedCardIndex] = editedCard;
             }
         },
-        toggle:(state,action)=>{
-            
+        // Reducer function (placeholder) for toggling card state
+        toggle: (state, action) => {
+            // Add toggle logic as needed
         }
     }
-})
+});
 
-export const {add,remove,edit,toggle} = cardsSlice.actions
-export default cardsSlice.reducer
+export const { add, remove, edit, toggle } = cardsSlice.actions;
+
+export default cardsSlice.reducer;
